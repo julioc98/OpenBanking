64165c337d26641d64de53a66409e0e682533936
julioc98 <julioc.santos98@gmail.com>
2020-12-06 23:19:04 -0300

    add html page callback

--- cmd/restapi/handlers/auth.go
+++ cmd/restapi/handlers/auth.go
@@ -36,20 +36,26 @@ func (ah *AuthHandler) Auth(w http.ResponseWriter, r *http.Request) {
 
 // Callback Auth
 func (ah *AuthHandler) Callback(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
 	code := r.URL.Query().Get("code")
 	log.Println("[DEBUG] Query Code:", code)
 	if code == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	accessToken, err := ah.authService.CallbackAuth(code)
+	_, err := ah.authService.CallbackAuth(code)
 	if err != nil {
 		log.Println("[ERROR] message:", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf(`{ "accessToken": "%s" }`, accessToken)))
+	// w.Write([]byte(fmt.Sprintf(`{ "accessToken": "%s" }`, accessToken)))
+	w.Write([]byte(`
+		<div style="display:flex; align-items: center;flex-direction: column;margin-top: 10%; font-weight: bold;">
+		<div>Você foi logado com sucesso!</div>
+		<div>Retorne para a aplicação.</div>
+		</div>`))
 }
